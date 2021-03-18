401aa4b41b403401bd134e62595842d3dccdc793
Dillon Barker <dor.barker@gmail.com>
2021-03-18 15:56:18 -0500

    untested first draft

--- tabulate_read_matches.py
+++ tabulate_read_matches.py
@@ -5,27 +5,37 @@
 # samfile.fetch() will return all reads overlapping the requested region,
 # including partial coverage
 #
-# Within pysam, coordinates are 0-based, half-open intervals
-# BAM is also 0-based, so maybe should be converted to that before this script
-# for least confusion
+# Pysam coordinates are 0-based, half-open intervals
+# BAM coordinates are 0-based
+# Reference positions are 1-based
 
-
-import pysam
 import argparse
-import itertools
+import pysam
 
 def arguments():
 
     parser = argparse.ArgumentParser()
 
-    parser = parser.add_argument('input')
+    parser = parser.add_argument('--bam')
+
+    parser = parser.add_argument('--reference')
+
+    parser = parser.add_argument('--mutations')
 
     return parser.parse_args()
 
 
 def main():
-    pass
 
+    args = arguments()
+
+    vocs = load_mutations(args.mutations)
+
+    reads = get_reads(args.bam, args.reference)
+
+    mutant_reads = find_mutations(reads, vocs)
+
+    format_report(mutant_reads)
 
 def load_mutations(mutations_path: str):
 
@@ -42,7 +52,9 @@ def load_mutations(mutations_path: str):
 
                 for snp in snps:
 
-                    position = int(snp[:-1])
+                    # Convert 1-based reference to
+                    # 0-based for BAM and pysam
+                    position = int(snp[:-1]) - 1
                     nucleotide = snp[-1]
 
                 groups[idx][position] = nucleotide
@@ -50,20 +62,64 @@ def load_mutations(mutations_path: str):
     return groups
 
 
-def match_reads_to_region():
-    pass
+def get_reads(bam_path: str, ref_path: str):
+
+    with pysam.AlignmentFile(bam_path,
+                             reference_filename=ref_path,
+                             mode='rb') as aln:
+        return list(aln)
 
 
-def is_mutant():
-    pass
+def find_mutations(reads, vocs):
+
+    results = {}
+
+    for variant in vocs.values():
+
+        start, *_, end = sorted(variant.keys())
+
+        results[variant] = find_variant_mutations(reads, variant)
+
+    return results
 
 
-def has_all_mutations():
-    pass
+def find_variant_mutations(reads, mutations):
 
 
-def format_report():
-    pass
+    results = {}
+
+    for read in reads:
+
+        results[read.query_name] = set()
+
+        seq = read.get_forward_sequence()
+
+        start = read.reference_start
+
+        for position in mutations:
+
+            read_offset = position - start
+
+            try:
+                has_mutation = seq[read_offset] == mutations[position]
+
+            except IndexError:
+                has_mutation = False
+
+            if has_mutation:
+                results[query_name].add(position)
+
+
+    return results
+
+
+def format_report(read_results):
+
+    only_mutant_reads = filter(lambda x: read_results[x], read_results)
+
+    sorted_reads = sorted(only_mutant_reads,
+                          key=lambda x: len(read_results[x]), reverse=True)
+
 
 def write_report():
     pass
