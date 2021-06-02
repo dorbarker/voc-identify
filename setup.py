e3138d91ab5fcd3ed75847f29962aa31f2ce6374
Dillon Barker <dor.barker@gmail.com>
2021-06-02 18:03:21 -0500

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     name="mmmvi",
     version=__version__,
     packages=find_packages(),
+    install_requires=["pandas", "pysam"],
     author="Dillon O.R. Barker",
     author_email="dillon.barker@canada.ca",
-    entry_points={"console_scripts": ["mmmvi=src.mmmvi:main",]},
+    entry_points={"console_scripts": ["mmmvi=src.mmmvi:main"]},
 )
