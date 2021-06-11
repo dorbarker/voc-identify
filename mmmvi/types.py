00b667b5fb8c9c38a44a35611ac93df2521fb561
Dillon Barker <dor.barker@gmail.com>
2021-06-11 16:19:42 -0500

    move reporting functions to new file

---
+++ mmmvi/types.py
@@ -0,0 +1,9 @@
+from typing import Dict, List, Tuple, Optional
+
+Position = Tuple[int, ...]
+Mutation = Tuple[Optional[str], ...]
+Mutations = Dict[Position, Mutation]
+VoCs = Dict[str, Mutations]
+Reads = List[pysam.AlignedSegment]
+MutationResults = Dict[str, List[Position]]
+VoCResults = Dict[str, MutationResults]
